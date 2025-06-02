65053a34f783fb99e1ccabe6c3f913b0296eadd3
Ariel Corte <cortearielformal@gmail.com>
2025-06-02 12:38:57 -0300

    mover archivos .s al directorio de cada ejercicio

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -7,47 +7,25 @@
 	.equ GPIO_GPLEV0,    0x34
 
 	.globl main
+	.include "formas.s"
+	.include "dibujar_numeros.s"
+	.include "sonic.s"
+  //.include "frame_1.s"
+	.include "frame_2.s"
+	//.include "frame_3.s"
+	//.include "frame_4.s"
 
 main:
-	// x0 contiene la direccion base del framebuffer
- 	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
+	mov x20, x0 // Guarda la dirección base-inicio del framebuffer en x20
+	mov x21, SCREEN_WIDTH // Guardo el largo de la pantalla x21
+	mov x22, SCREEN_HEIGH // Guardo el ancho de la pantalla x22
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
-
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
-
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
-
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
-
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
-
-	//---------------------------------------------------------------
-	// Infinite Loop
-
+	bl Fondo
+	bl Numeros
+	bl Sonic_Base
+	//bl Sonic_Frame_1
+  bl Sonic_Frame_2
+	//bl Sonic_Frame_3
+	//bl Sonic_Frame_4
 InfLoop:
 	b InfLoop
---
+++ ejercicio2/frame_4.s
@@ -0,0 +1,316 @@
+.ifndef frame_1_s
+
+.include "formas.s"
+
+.equ BLANCO, 0xFFFFFFFF
+.equ NEGRO, 0x00000000
+.equ AZUL_1, 0x00001C86
+.equ AZUL_2, 0x00002991
+.equ AZUL_3, 0x00003DAC
+.equ ANIL_1, 0x003232e6
+.equ ANIL_2, 0x005858e0
+.equ GRIS_2, 0x00D3D3D3
+.equ GRIS_3, 0x00B8B8B8
+.equ GRIS_1, 0x00636363
+.equ PIEL_2, 0x00E6A183
+.equ PIEL_1, 0x00c68642
+.equ ROJO_1, 0x008B0000
+.equ ROJO_2, 0x00d90000
+.equ ROJO_3, 0x00a60000
+.equ ROJO_4, 0x00590000
+
+Sonic_Frame_4:
+  mov x2, 25
+	mov x3, 5
+	mov x0, 230
+	mov x1, 240
+ 	movz x4, AZUL_2 & 0xFFFF, lsl 0
+ 	movk x4, (AZUL_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 20
+	mov x0, 305
+	mov x1, 250
+ 	movz x4, AZUL_1 & 0xFFFF, lsl 0
+ 	movk x4, (AZUL_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 10
+	mov x0, 300
+	mov x1, 255
+ 	movz x4, AZUL_1 & 0xFFFF, lsl 0
+ 	movk x4, (AZUL_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 20
+	mov x3, 15
+	mov x0, 185
+	mov x1, 325
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 20
+	mov x3, 15
+	mov x0, 175
+	mov x1, 330
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 15
+	mov x3, 15
+	mov x0, 170
+	mov x1, 335
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 15
+	mov x3, 15
+	mov x0, 165
+	mov x1, 340
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 15
+	mov x3, 20
+	mov x0, 160
+	mov x1, 345
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+  
+  mov x2, 15
+	mov x3, 20
+	mov x0, 155
+	mov x1, 360
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 10
+	mov x3, 5
+	mov x0, 155
+	mov x1, 380
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 15
+	mov x0, 160
+	mov x1, 380
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x0, 165
+	mov x1, 395
+	mov x2, 5
+	mov x3, ROJO_2
+	bl Cuadrado
+
+  mov x2, 20
+	mov x3, 5
+	mov x0, 210
+	mov x1, 370
+ 	movz x4, ANIL_1 & 0xFFFF, lsl 0
+ 	movk x4, (ANIL_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 10
+	mov x3, 15
+	mov x0, 210
+	mov x1, 375
+ 	movz x4, ANIL_1 & 0xFFFF, lsl 0
+ 	movk x4, (ANIL_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 15
+	mov x3, 5
+	mov x0, 200
+	mov x1, 390
+ 	movz x4, ANIL_1 & 0xFFFF, lsl 0
+ 	movk x4, (ANIL_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 20
+	mov x3, 5
+	mov x0, 195
+	mov x1, 395
+ 	movz x4, BLANCO & 0xFFFF, lsl 0
+ 	movk x4, (BLANCO >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 25
+	mov x3, 5
+	mov x0, 195
+	mov x1, 400
+ 	movz x4, GRIS_3 & 0xFFFF, lsl 0
+ 	movk x4, (GRIS_3 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 10
+	mov x3, 5
+	mov x0, 205
+	mov x1, 400
+ 	movz x4, BLANCO & 0xFFFF, lsl 0
+ 	movk x4, (BLANCO >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 25
+	mov x3, 5
+	mov x0, 190
+	mov x1, 405
+ 	movz x4, GRIS_3 & 0xFFFF, lsl 0
+ 	movk x4, (GRIS_3 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 50
+	mov x3, 10
+	mov x0, 185
+	mov x1, 410
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 45
+	mov x3, 10
+	mov x0, 190
+	mov x1, 415
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 20
+	mov x3, 5
+	mov x0, 235
+	mov x1, 415
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 35
+	mov x3, 10
+	mov x0, 235
+	mov x1, 405
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 30
+	mov x3, 10
+	mov x0, 245
+	mov x1, 400
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 25
+	mov x3, 10
+	mov x0, 255
+	mov x1, 395
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+  
+  mov x2, 25
+	mov x3, 10
+	mov x0, 255
+	mov x1, 395
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 20
+	mov x3, 5
+	mov x0, 265
+	mov x1, 390
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 10
+	mov x3, 5
+	mov x0, 265
+	mov x1, 385
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 15
+	mov x0, 275
+	mov x1, 375
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 25
+	mov x0, 290
+	mov x1, 360
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 15
+	mov x0, 285
+	mov x1, 375
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 5
+	mov x3, 15
+	mov x0, 280
+	mov x1, 385
+ 	movz x4, ROJO_1 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_1 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 45
+	mov x3, 5
+	mov x0, 190
+	mov x1, 420
+ 	movz x4, ROJO_4 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_4 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x0, 185
+	mov x1, 415
+	mov x2, 5
+	mov x3, ROJO_4
+	bl Cuadrado
+
+  mov x2, 10
+	mov x3, 5
+	mov x0, 210
+	mov x1, 415
+ 	movz x4, GRIS_3 & 0xFFFF, lsl 0
+ 	movk x4, (GRIS_3 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 10
+	mov x3, 5
+	mov x0, 215
+	mov x1, 410
+ 	movz x4, BLANCO & 0xFFFF, lsl 0
+ 	movk x4, (BLANCO >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+  mov x2, 15
+	mov x3, 5
+	mov x0, 215
+	mov x1, 405
+ 	movz x4, ROJO_2 & 0xFFFF, lsl 0
+ 	movk x4, (ROJO_2 >> 16) & 0xFFFF, lsl 16 
+  bl Rectangulo
+
+.endif 
+
